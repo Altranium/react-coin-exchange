19c9df6b6a78f069ed83701a06391da0a24f6295
Alan Tran <dev@protoalpha.co>
2021-02-04 15:18:25 -0600

    Modify to create a table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Coin from './components/Coin/Coin';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="App-title">
+          Coin Exchange
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <table className="coin-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Coin name="Bitcoin" ticker="BTC" price={9999.99}/>
+          <Coin name="Ethereum" ticker="ETC" price={299.99}/>
+          <Coin name="Tether" ticker="USDT" price={1}/>
+          <Coin name="Ripple" ticker="XRP" price={0.2}/>
+        </tbody>
+      </table>
     </div>
   );
 }
